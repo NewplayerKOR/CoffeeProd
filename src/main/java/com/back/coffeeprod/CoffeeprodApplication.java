50d1496d79d7e04635f19e16b7caf65036efb1f6
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2026-03-17 01:07:35 +0900

    초기 환경 세팅

---
+++ src/main/java/com/back/coffeeprod/CoffeeprodApplication.java
@@ -0,0 +1,13 @@
+package com.back.coffeeprod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeprodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeprodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/back/coffeeprod/CoffeeprodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.coffeeprod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeprodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
