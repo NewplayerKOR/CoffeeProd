1b377387582cac38e04b3d4c7291693cab827a09
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2026-04-27 20:24:26 +0900

    Redis 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,49 @@
 plugins {
-	java
-	id("org.springframework.boot") version "4.1.0-SNAPSHOT"
-	id("io.spring.dependency-management") version "1.1.7"
+    java
+    id("org.springframework.boot") version "4.1.0-SNAPSHOT"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "com.back"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(21)
-	}
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 configurations {
-	compileOnly {
-		extendsFrom(configurations.annotationProcessor.get())
-	}
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
 }
 
 repositories {
-	mavenCentral()
-	maven { url = uri("https://repo.spring.io/snapshot") }
+    mavenCentral()
+    maven { url = uri("https://repo.spring.io/snapshot") }
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-h2console")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.postgresql:postgresql:42.7.10") // PostgreSQL JDBC 드라이버
-	implementation("org.springframework.boot:spring-boot-starter-security:4.1.0-M2")
-	implementation("io.jsonwebtoken:jjwt-api:0.13.0") // JWT 라이브러리
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2") // Swagger
-	compileOnly("org.projectlombok:lombok")
-	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	runtimeOnly("com.h2database:h2")
-	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
-	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0") 
-	annotationProcessor("org.projectlombok:lombok")
-	testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
-	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-h2console")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.postgresql:postgresql:42.7.10") // PostgreSQL JDBC 드라이버
+    implementation("org.springframework.boot:spring-boot-starter-security:4.1.0-M2")
+    implementation("io.jsonwebtoken:jjwt-api:0.13.0") // JWT 라이브러리
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2") // Swagger
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:4.0.6")    // Redis
+    compileOnly("org.projectlombok:lombok")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.13.0")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.13.0")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
