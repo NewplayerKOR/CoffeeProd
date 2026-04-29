45bd995debc2ba0717188b500eae04556052b80b
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2026-04-29 23:18:33 +0900

    Feat#54 Cart, CartItem 관련 Entity, Repository 구현 (#55)
    * GrindType ENUM 생성
    
    * Cart.java 생성
    
    * CartItem.java 생성
    
    * CartRepository 생성
    
    * CartItemRepository 생성

---
+++ src/main/java/com/back/coffeeprod/domain/cart/entity/GrindType.java
@@ -0,0 +1,8 @@
+package com.back.coffeeprod.domain.cart.entity;
+
+public enum GrindType {
+    WHOLE_BEAN, // 홀빈 (분쇄 안 함)
+    ESPRESSO, // 에스프레소
+    DRIP,     // 드립용 분쇄
+    FRENCH_PRESS // 프렌치프레스용 분쇄
+}
