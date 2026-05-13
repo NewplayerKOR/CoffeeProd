37b82c835195b30a2e100538768fab886e4c3ee1
NewplayerKOR <parktaeque@gmail.com>
2026-05-13 22:32:24 +0900

    PaymentDto 수정

--- src/main/java/com/back/coffeeprod/domain/payment/dto/PaymentDto.java
+++ src/main/java/com/back/coffeeprod/domain/payment/dto/PaymentDto.java
@@ -1,5 +1,6 @@
-package com.back.coffeeprod.domain.payment.order;
+package com.back.coffeeprod.domain.payment.dto;
 
+import com.back.coffeeprod.domain.payment.entity.Payment;
 import com.back.coffeeprod.domain.payment.entity.PaymentStatus;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,13 @@ import java.time.LocalDateTime;
 public class PaymentDto {
 
     // 결제 승인 요청 DTO
+    // 토스에서 클라이언트로 전달된 3가지 값을 서버로 전송
     @Getter
     @NoArgsConstructor
     public static class ConfirmRequest {
-        private String paymentKey;  // 토스 발급 결제 고유 키
-        private Long orderId;       // 우리 서버의 주문 ID
-        private int amount;         // 결제 금액 (위변조 검증 대상)
+        private String paymentKey; // 토스 발급 결제 고유 키
+        private Long orderId;      // 우리 서버의 주문 ID
+        private int amount;        // 결제 금액 (위변조 검증 대상)
     }
 
     // 결제 응답 DTO
@@ -28,7 +30,7 @@ public class PaymentDto {
         private final PaymentStatus status;
         private final LocalDateTime paidAt;
 
-        public Response(com.back.coffeeprod.domain.payment.entity.Payment payment) {
+        public Response(Payment payment) {
             this.paymentId = payment.getId();
             this.orderId = payment.getOrders().getId();
             this.pgProvider = payment.getPgProvider();
