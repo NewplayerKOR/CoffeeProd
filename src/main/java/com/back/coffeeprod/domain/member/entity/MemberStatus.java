dbef007d0ce3cb01465da3a7e6f7dc2e4e220a6b
NewplayerKOR <parktaeque@gmail.com>
2026-06-01 13:56:24 +0900

    MemberStatus 내용추가(SUSPENDED회원)

--- src/main/java/com/back/coffeeprod/domain/member/entity/MemberStatus.java
+++ src/main/java/com/back/coffeeprod/domain/member/entity/MemberStatus.java
@@ -1,5 +1,7 @@
 package com.back.coffeeprod.domain.member.entity;
 
 public enum MemberStatus {
-    ACTIVE, WITHDRAWN
+    ACTIVE, // 정상회원
+    SUSPENDED, // 관리자에 의해 정지된 회원
+    WITHDRAWN // 탈퇴 회원
 }
