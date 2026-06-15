c1c49c9b5c72f449b97397f283cc6c20cdb4f1ed
NewplayerKOR <parktaeque@gmail.com>
2026-06-15 18:45:22 +0900

    잘못된 import 수정

--- src/main/java/com/back/coffeeprod/global/security/handler/RestAccessDeniedHandler.java
+++ src/main/java/com/back/coffeeprod/global/security/handler/RestAccessDeniedHandler.java
@@ -2,7 +2,7 @@ package com.back.coffeeprod.global.security.handler;
 
 import com.back.coffeeprod.global.common.CommonResponse;
 import com.back.coffeeprod.global.exception.ErrorCode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
