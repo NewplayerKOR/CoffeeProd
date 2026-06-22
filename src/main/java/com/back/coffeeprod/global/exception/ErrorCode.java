b49ea1fc70a6e90d43d7741aba2e5b8e707d97e0
NewplayerKOR <parktaeque@gmail.com>
2026-06-22 15:13:23 +0900

    매출 통계 관련 ErrorCode 추가

--- src/main/java/com/back/coffeeprod/global/exception/ErrorCode.java
+++ src/main/java/com/back/coffeeprod/global/exception/ErrorCode.java
@@ -61,6 +61,9 @@ public enum ErrorCode {
     QNA_ACCESS_DENIED(403, "본인의 QnA만 접근할 수 있습니다."),
     QNA_ALREADY_ANSWERED(409, "답변이 완료된 QnA입니다."),
 
+    // [매출 통계 관련]
+    INVALID_STATISTICS_DATE_RANGE(400, "매출 통계 시작일은 종료일보다 늦을 수 없습니다."),
+    STATISTICS_DATE_RANGE_TOO_LARGE(400, "매출 통계 재집계 기간은 최대 365일까지 가능합니다."),
 
     // [공통 에러]
     INVALID_INPUT_VALUE(400, "잘못된 입력 값입니다."),
