10d92a43c00f90715f207b9182ca8d8b820dea49
NewplayerKOR <parktaeque@gmail.com>
2026-07-10 15:13:06 +0900

    ProductService return 필드 오류 수정

--- src/main/java/com/back/coffeeprod/domain/product/service/ProductService.java
+++ src/main/java/com/back/coffeeprod/domain/product/service/ProductService.java
@@ -78,7 +78,7 @@ public class ProductService {
 
         // 관리자는 ON_SALE, SOLD_OUT, HIDDEN 상품을 모두 조회 가능
         return productRepository
-                .findAllWithFilters(categoryId, coffeeProfileId, coffeeProfileId, beanType, decaf, roastLevel, status, keyword, pageable)
+                .findAllWithFilters(categoryId, coffeeProfileId, processingMethodId, beanType, decaf, roastLevel, status, keyword, pageable)
                 .map(ProductDto.SummaryResponse::new);
     }
 
